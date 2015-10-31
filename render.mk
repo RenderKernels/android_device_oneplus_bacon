7b3825ffea34faf3acb73524b6f9b9fc1caf54e6
Snuzzo <jrracinfan@gmail.com>
2015-10-31 12:45:15 -0400

    Define Zip files dir for Anykernel installer

--- render.mk
+++ render.mk
@@ -6,3 +6,5 @@ PRODUCT_KERNEL_SOURCE := kernel/oneplus/bacon
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
 ZIMAGE := arch/arm/boot/zImage
+
+ZIP_FILES_DIR := device/oneplus/bacon/anykernel_installer
