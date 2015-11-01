f965f3b031d2e382133efdd023a15d679859d067
Snuzzo <jrracinfan@gmail.com>
2015-11-01 11:56:59 -0500

    Move ZIMAGE definition

--- render.mk
+++ render.mk
@@ -5,7 +5,6 @@ PRODUCT_DEFCONFIG := render_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/oneplus/bacon
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
 ARCH := arm
-ZIMAGE := arch/arm/boot/zImage
 
 ZIP_FILES_DIR := device/oneplus/bacon/anykernel_installer
 TARGET_REQUIRES_DTB := true
