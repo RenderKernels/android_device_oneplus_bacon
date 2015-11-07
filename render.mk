6948f6b38aa0c366bbe5d1119df1d30ca90a734b
Snuzzo <jrracinfan@gmail.com>
2015-11-07 15:46:57 -0500

    Revert "ARCH: Use default define"
    This reverts commit 526739ae3654a821c026a441a7a3d7090ef26fb2.

--- render.mk
+++ render.mk
@@ -4,6 +4,7 @@ PRODUCT_NAME := render_bacon
 PRODUCT_DEFCONFIG := render_defconfig
 PRODUCT_KERNEL_SOURCE := kernel/oneplus/bacon
 CROSS_COMPILE := $(ANDROID_BUILD_TOP)/toolchains/arm-eabi-5.2/bin/arm-eabi-
+ARCH := arm
 
 ZIP_FILES_DIR := device/oneplus/bacon/anykernel_installer
 TARGET_REQUIRES_DTB := true
